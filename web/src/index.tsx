d4919c9bb383b0436fbf3b572c343c031e669d7f
brecert <11599528+Brecert@users.noreply.github.com>
2021-04-09 07:24:44 -0400

    lilliepaad-web: stop re-rendering entire message list every update

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,9 @@
 import './css'
 
 import { h } from 'sinuous'
-import { o, subscribe } from 'sinuous/observable'
+import { map } from 'sinuous/map'
+import { o, Observable, subscribe } from 'sinuous/observable'
+
 import { Search } from './components/search'
 import { Tab } from './components/tab'
 import { Fragment } from './components/fragment'
@@ -9,6 +11,7 @@ import { InputArea } from './components/input-area'
 import { Message } from './components/message'
 import { Item } from './components/item'
 import styles from './styles/app.module.css'
+
 import { scrollToBottom } from './utils.js'
 Fragment // use fragment so it's not removed
 
@@ -29,20 +32,61 @@ const searchHistory = o(
     ))
 )
 
-const DefaultMessage = () => (
-  <Message
-    displayName="bree"
-    icon="https://avatars1.githubusercontent.com/u/11599528?s=48"
-    time={Date.now()}
-  >
-    hello world
-  </Message>
+// todo: move these to a shared types module
+interface IUser {
+  id: number
+  username: string
+  avatar_url: string
+}
+
+interface IContent {
+  text: string
+}
+
+interface IMessage {
+  id: number
+  timestamp: number
+  content: IContent
+  author: IUser
+}
+
+const default_message = (id: number): IMessage => ({
+  id: id,
+  timestamp: Date.now(),
+  content: {
+    text: 'Hello world!',
+  },
+  author: {
+    id: 0,
+    username: 'bree',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/11599528?s=48',
+  },
+})
+
+const messages: Observable<IMessage[]> = o(
+  Array(10)
+    .fill(0)
+    .map((_, i) => default_message(i))
+)
+const Messages = (
+  <div class={styles.messages}>
+    {map(messages, (msg) => (
+      <Message
+        data-key={msg.id}
+        displayName={msg.author.username}
+        icon={msg.author.avatar_url}
+        time={msg.timestamp}
+      >
+        {msg.content.text}
+      </Message>
+    ))}
+  </div>
 )
 
-const messages = o(Array(10).fill(0).map(DefaultMessage))
-const Messages = <div class={styles.messages}>{messages}</div>
-
-setInterval(() => messages(messages().concat([DefaultMessage()])), 2000)
+setInterval(
+  () => messages(messages().concat([default_message(Date.now())])),
+  1000
+)
 
 subscribe(() => {
   messages()
