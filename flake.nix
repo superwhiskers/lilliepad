339b2fd82021b30d359dc147645f535e0e1512dd
superwhiskers <whiskerdev@protonmail.com>
2021-04-16 00:38:56 -0500

    lilliepad: change nix-shell based system to a flake-based one

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "development shell for lilliepad";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustup flatbuffers yarn ];
+        };
+      });
+}
